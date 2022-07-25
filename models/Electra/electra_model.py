1b5970fdf6cbe2b6ffcca872f6c73aecf4b4c9ef
alzoqm <70330480+alzoqm@users.noreply.github.com>
2022-07-25 17:53:04 +0900

    Update electra_model.py

--- models/Electra/electra_model.py
+++ models/Electra/electra_model.py
@@ -28,7 +28,7 @@ class MultiHeadAttention(tf.keras.Model):
     self.scale = 1 / (self.depth ** 0.5)
 
   def call(self, inputs, mask=None):
-    B, L, D = tf.shape(inputs)
+    B, L, D = inputs.shape
 
     q = self.q_linear(inputs)
     k = self.k_linear(inputs)
@@ -100,7 +100,7 @@ class GeneratorEncoder(tf.keras.Model):
     super().__init__()
     # self.word_emb = tf.keras.layers.Embedding(vocab_size, emb_size)
     # self.seg_emb = tf.keras.layers.Embedding(seg_type, emb_size)
-    # self.pos_emb = tf.keras.layers.Embedding(max_len + 1, emb_size) #Discriminator와 동일한 임베딩을 사용
+    # self.pos_emb = tf.keras.layers.Embedding(max_len + 1, emb_size) #Discriminator와 동일한 임베딩을 사용 -> 더 좋은 성능을 보여줌
 
     self.encoder_layers = [EncoderLayer(dff, d_model, num_heads, dropout) for i in range(num_layers)]
     self.emb_proj = tf.keras.layers.Dense(d_model)
@@ -112,7 +112,8 @@ class GeneratorEncoder(tf.keras.Model):
   def call(self, inputs, segments, word_emb, seg_emb, pos_emb):
     batch_size = tf.shape(inputs)[0]
     pos = np.arange(inputs.shape[1]) + 1
-    pos = np.broadcast_to(pos, shape=(batch_size, inputs.shape[1]))
+    pos = tf.broadcast_to(pos, shape=(batch_size, inputs.shape[1]))
+    #pos = np.broadcast_to(pos, shape=(batch_size, inputs.shape[1]))
     pos_mask = tf.math.equal(inputs, 0)
     pos = tf.where(~pos_mask, x=pos, y=0) 
 
@@ -145,7 +146,8 @@ class DiscriminatorEncoder(tf.keras.Model):
   def call(self, inputs, segments):
     batch_size = tf.shape(inputs)[0]
     pos = np.arange(inputs.shape[1]) + 1
-    pos = np.broadcast_to(pos, shape=(batch_size, inputs.shape[1]))
+    pos = tf.broadcast_to(pos, shape=(batch_size, inputs.shape[1]))
+    #pos = np.broadcast_to(pos, shape=(batch_size, inputs.shape[1]))
     pos_mask = tf.math.equal(inputs, 0)
     pos = tf.where(~pos_mask, x=pos, y=0) 
 
@@ -219,43 +221,48 @@ class ElectraPretrain(tf.keras.Model):
     self.gen_linear_cls = tf.keras.layers.Dense(config['num_classes'], use_bias=False)
     self.gen_linear_lm = tf.keras.layers.Dense(config['vocab_size'], use_bias=False)
 
-    self.dis_linear = tf.keras.layers.Dense(2)
+    self.dis_linear = tf.keras.layers.Dense(2, activation='sigmoid') #시그모이드 활성화 함수를 활용해 마지막 분류
+    self.dis_lambda = config['dis_lambda']
 
   def gen_loss_function(self, labels_lm, labels_cls, logits_lm, logits_cls, inputs):
-    mask = tf.math.not_equal(labels_lm, -1)  #-1이 아닐 경우 True
+    mask = tf.math.not_equal(labels_lm, -1)  # masking한 곳은 True
     mask_float = tf.cast(mask, dtype=tf.float32)
     labels_lm_custom = tf.where(mask, x=labels_lm, y=inputs)  #mask가 True인 곳을 lm으로, 아닌 곳은 inputs -> -1일 모두 제거
 
-    loss_fn_cls = tf.losses.SparseCategoricalCrossentropy(reduction='none')
-    loss_fn_lm = tf.losses.SparseCategoricalCrossentropy(reduction='none')
-
-    loss_cls = loss_fn_cls(labels_cls, logits_cls)
+    loss_cls = tf.losses.sparse_categorical_crossentropy(labels_cls, logits_cls)
     loss_cls = tf.reshape(loss_cls, shape=(-1, 1))
-    loss_lm = loss_fn_lm(labels_lm_custom, logits_lm)
+    loss_lm = tf.losses.sparse_categorical_crossentropy(labels_lm_custom, logits_lm)
 
-    loss_lm = tf.multiply(loss_lm, mask_float)  #labels_lm이 -1인 mask의 값은 0이다. 따라서 labels_lm이 값이 -1인 loss는 0으로 만들어준다.
+    loss_lm = tf.multiply(loss_lm, mask_float)  #masking한 위치의 mask_float 값은 1, 아닌 곳은 0이다.
     loss = loss_lm + loss_cls
     return loss, mask, labels_lm_custom  #mask와 labels_lm_custom은 discriminator의 input을 만드는 sampling 과정에 사용하기 위해
 
-  def call(self, inputs, segments, labels_cls, labels_lm):
-    gen_outputs, gen_outputs_cls, gen_attn_mat_list = self.generator(inputs, segments, self.word_emb, self.seg_emb, self.pos_emb)
+  def call(self, inputs):
+    input, segments, labels_cls, labels_lm = inputs
+    gen_outputs, gen_outputs_cls, gen_attn_mat_list = self.generator(input, segments, self.word_emb, self.seg_emb, self.pos_emb)
     gen_outputs_cls = self.gen_linear_cls(gen_outputs_cls)  #앞 뒤의 문장이 동일한 연결이 되는 문장인지 판별
     gen_outputs = self.gen_linear_lm(gen_outputs)
 
-    gen_loss, mask, labels_lm_custom = self.gen_loss_function(labels_lm, labels_cls, gen_outputs, gen_outputs_cls, inputs)
+    gen_loss, mask, labels_lm_custom = self.gen_loss_function(labels_lm, labels_cls, gen_outputs, gen_outputs_cls, input)
     ##sampling
     #labels_lm_custom -> 모든 문장이 다 들어가있음
     gen_outputs_softmax = tf.nn.softmax(gen_outputs, axis=2)
-    sampling = []
-    for one_batch in gen_outputs_softmax:
+    sampling = tf.constant([], dtype=tf.int32)
+    #for index, one_batch in enumerate(gen_outputs_softmax):
+    for index in range(tf.shape(gen_outputs_softmax)[0]):
       """
-      softmax 확률에 따른 sampling을 진행, 2차원 텐서로 입력을 받기 때문에,
+      softmax 확률에 따른 sampling을 진행, 2차원 텐서로만 입력을 받기 때문에,
        for문을 통해 batch size만큼 반복(더 효과적인 방법 찾아봐야 할 듯)
       """
-      one_batch_sampling = tf.random.categorical(one_batch, 1, dtype=tf.int32) 
-      sampling.append(one_batch_sampling)
-    sampling = np.array(sampling)
-    sampling = tf.convert_to_tensor(sampling)
+      one_batch_sampling = tf.random.categorical(gen_outputs_softmax[index], 1, dtype=tf.int32)
+      one_batch_sampling = tf.expand_dims(one_batch_sampling, axis=0)
+      if index == 0: #-> predict할 때 문제 발생(해결해야함)
+        sampling = one_batch_sampling
+      else:
+        sampling = tf.concat([sampling, one_batch_sampling], axis=0)
+      #sampling.append(one_batch_sampling)
+    #sampling = np.array(sampling)
+    #sampling = tf.convert_to_tensor(sampling)
     sampling = tf.squeeze(sampling, axis=2)
 
     sampling = tf.where(mask, x=sampling, y = labels_lm_custom) # mask로 대체된 token을 sampling 값으로 변경
@@ -271,12 +278,14 @@ class ElectraPretrain(tf.keras.Model):
     dis_outputs = self.dis_linear(dis_outputs)
 
     dis_loss = tf.losses.sparse_categorical_crossentropy(same_token_mask, dis_outputs)
+    total_loss = gen_loss + dis_loss*self.dis_lambda
 
-    return gen_loss, sampling, dis_loss
+    return total_loss, gen_loss, sampling, dis_loss
 
 ##다른 모델 2개로 수정 필요
 
 if __name__ == '__main__':
+  #model hyper parameter는 electra small++과 동일하게 설정
   config = {}
   config['num_classes'] = 2
   config['max_len'] = 512
@@ -294,19 +303,17 @@ if __name__ == '__main__':
   config['dis_emb_size'] = 128
   config['dis_num_heads'] = 4
   config['dis_dropout'] = 0.1
-
+  config['dis_lambda'] = 50
 
   electra_pretrain = ElectraPretrain(config)
 
-  x = tf.range(512)
-  x = tf.expand_dims(x, axis=0)
+  x = tf.range(512*10)
   x = tf.reshape(x, shape=(-1, 512))
   test_batch_size = x.shape[0]
   seg = tf.ones_like(x)
   label_cls = tf.ones(shape=(test_batch_size, ))
-  gen_loss, sampling, dis_loss = electra_pretrain(x, seg, label_cls, x)
+  total_loss, gen_loss, sampling, dis_loss = electra_pretrain([x, seg, label_cls, x])
   print(gen_loss.shape)
   print(dis_loss.shape)
-
 
 
